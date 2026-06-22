7f9213eb2e1b4d04b6f4e4af16f8bf7ed9f7e53e
CaduESG <cadu_sgriebeler@hotmail.com>
2026-06-22 18:28:25 -0300

    Menu pronto (falta criar os controles)

--- menu.gd
+++ menu.gd
@@ -3,9 +3,24 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	#for buttons in get_tree().get_nodes_in_group("buttons"):
+		#buttons.connect("pressed", self, "on_button_pressed", [buttons])
+		#buttons.connect("mouse_exited", self, "mouse_interaction", [buttons, "exited"])
+		#buttons.connect("mouse_entered", self, "mouse_interaction", [buttons, "entered"])
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_iniciar_jogo_pressed() -> void:
+	var _game: bool = get_tree().change_scene_to_file("res://level_1.tscn")
+
+
+func _on_controles_pressed() -> void:
+	var _controls: bool = get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_sair_pressed() -> void:
+	get_tree().quit()
